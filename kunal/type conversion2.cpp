fc94f985aca14535798a5e566aa46dbe98e70ba4
kunal121 <kunalkalra121@gmail.com>
2017-02-18 19:18:45 +0530

    another question on type conversion

---
+++ kunal/type conversion2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class str
+{
+   char name[20];
+
+   public:
+   char *s;
+   str()
+   {
+
+   }
+   str(char name1[20])
+   {
+       strcpy(name,name1);
+   }
+   void display()
+   {
+       cout<<name;
+   }
+   void getdata()
+   {
+        s=new char[20];
+       cin>>s;
+   }
+   operator char*()
+   {
+       return s;
+   }
+};
+int main ()
+{
+    str s1;
+    char name[20];
+    cin>>name;
+    s1=name;
+    s1.display();
+    str s2;
+    s2.getdata();
+    char *desig;
+    desig=s2;
+    cout<<desig;
+}
