56ad31cd75814d139536e396d0e1d7c505beaec9
kunal121 <kunalkalra121@gmail.com>
2017-02-28 23:42:16 +0530

    swapping using call by value

---
+++ kunal/swap.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class swap1
+{
+public:
+    void swap2(int x,int y)
+    {
+        int z;
+        z=x;
+        x=y;
+        y=z;
+        cout<<x<<y;
+    }
+
+};
+int main()
+{
+    int x,y;
+    swap1 s;
+    cin>>x>>y;
+    s.swap2(x,y);
+
+}
