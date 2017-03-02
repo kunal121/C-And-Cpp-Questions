94253a71acfe454308c43f69f0ea89f923839fe6
Manan Garg <manangarg899@gmail.com>
2017-03-02 17:30:27 +0530

    Multilevel inheritance having multiple base class

---
+++ Manan/Multilevel_Inheritance(Multiple base classes).cpp
@@ -0,0 +1,57 @@
+//Multiple base class in inheritance
+#include<iostream>
+using namespace std;
+class student
+{
+    char name[20];
+    int rollno;
+    char branch[20];
+public:
+    student()
+    {
+        cout<<"Enter name rollno and branch:";
+        cin>>name;
+        cin>>rollno;
+        cin>>branch;
+    }
+    void display()
+    {
+     cout<<name<<"\n"<<rollno<<"\n"<<branch<<"\n";
+    }
+};
+class marks
+{
+    int mark[3];
+public:
+    marks()
+    {
+        cout<<"Enter marks of 3 subjects:";
+        for(int i=0;i<3;i++)
+            {
+                cin>>mark[i];
+        }
+    }
+    void display()
+    {
+        for(int i=0;i<3;i++)
+            {
+                cout<<mark[i]<<"\n";
+        }
+    }
+};
+class result:protected student,protected marks
+{
+public:
+    result()
+    {}
+    void display()
+    {
+        student::display();
+        marks::display();
+    }
+};
+int main()
+{
+    result r;
+    r.display();
+}
