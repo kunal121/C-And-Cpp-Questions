4197bfb8a81a4a21d6e3a500a26f7b45348af9cd
Manan Garg <manangarg899@gmail.com>
2017-03-02 17:25:29 +0530

    Inheriting private members using protected inheritance

---
+++ Manan/Multi_level_inheritance(protected).cpp
@@ -0,0 +1,57 @@
+//Inheriting private members using protected inheritance
+#include<iostream>
+using namespace std;
+class student
+{
+    char name[20];
+    int rollno;
+    char branch[20];
+public:
+    student()
+    {
+        cout<<"Enter name rollno and branch:";
+        cin>>name;
+        cin>>rollno;
+        cin>>branch;
+    }
+    void display()
+    {
+     cout<<name<<"\n"<<rollno<<"\n"<<branch<<"\n";
+    }
+};
+class marks:protected student
+{
+    int mark[3];
+public:
+    marks()
+    {
+        cout<<"Enter marks of 3 subjects:";
+        for(int i=0;i<3;i++)
+            {
+                cin>>mark[i];
+        }
+    }
+    void display()
+    {
+        for(int i=0;i<3;i++)
+            {
+                cout<<mark[i]<<"\n";
+        }
+    }
+};
+class result:protected marks
+{
+public:
+    result()
+    {}
+    void display()
+    {
+        student::display();
+        marks::display();
+    }
+};
+int main()
+{
+    result r;
+    r.display();
+}
