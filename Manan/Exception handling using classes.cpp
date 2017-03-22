af6bef23ae3cc701d0e028e5e082b65809b93ff1
Manan Garg <manangarg899@gmail.com>
2017-03-22 10:15:56 +0530

    Exception Handling

---
+++ Manan/Exception handling using classes.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class abc
+{
+    int a,b;
+public:
+    void input(int x,int y)
+    {
+        a=x;
+        b=y;
+    }
+    void dis1()
+    {
+        cout<<a<<endl<<b;
+    }
+    void dis2()
+    {
+        cout<<"Wrong inputs";
+    }
+};
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    abc o;
+    try
+    {
+        if(a==0 && b==0)
+            throw o;
+        else{
+            o.input(a,b);
+            o.dis1();
+        }
+    }
+    catch(abc &o1)
+    {
+        o1.dis2();
+    }
+}
