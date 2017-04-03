0c38a349dfdbec87c22a47ef4f2ada63c46822a8
manik chhabra <manikchhabra1275@gmail.com>
2017-04-03 20:53:41 +0530

    Delete decimal_to_hexadecimal_conversion.c.c

--- Manik Chhabra/c practice/decimal_to_hexadecimal_conversion.c.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  long int decimalNumber;
-  printf("Enter any decimal number: ");
-  scanf("%d",&decimalNumber);
-  printf("Equivalent hexadecimal number is: %X",decimalNumber);
-  return 0;
- }
