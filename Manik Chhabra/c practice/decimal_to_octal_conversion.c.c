4daed4f2faae56c992a56a04d7245c0d2e3d019a
manik <manikchhabra1275@gmail.com>
2017-04-03 20:40:56 +0530

    added new folder

---
+++ Manik Chhabra/c practice/decimal_to_octal_conversion.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main()
+{
+   long int decimalNumber;
+   printf("Enter any decimal number : ");
+   scanf("%d",&decimalNumber);
+   printf("Equivalent octal number is: %o",decimalNumber);
+   return 0;
+}
+
---
+++ Manik Chhabra/c practice/swapping_of_2_nos..c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int i,k;
+    printf("enter first no.");
+    scanf("%d",&i);
+    printf("enter second no.");
+    scanf("%d",&k);
+    printf("value of i=%d k=%d before swapping\n", i, k);
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping", i, k);
+
+    return 0;
+}
