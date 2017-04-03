748d57ff621a8fc49c394114363653f23f66a04c
manik <manikchhabra1275@gmail.com>
2017-04-03 20:46:29 +0530

    decimal to hexadecimal conversion

---
+++ Manik Chhabra/c practice/decimal_to_hexadecimal_conversion.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  long int decimalNumber;
+  printf("Enter any decimal number: ");
+  scanf("%d",&decimalNumber);
+  printf("Equivalent hexadecimal number is: %X",decimalNumber);
+ }
