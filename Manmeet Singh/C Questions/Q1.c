5626a0afd2a20274b97712dd6328836011f791f9
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-04-06 12:13:25 -0600

    Q1 updated

--- Manmeet Singh/C Questions/Q1.c
+++ Manmeet Singh/C Questions/Q1.c
@@ -1,16 +1,7 @@
-#include <stdio.h>
-main()
+#include<stdio.h>
+#include<limits.h>
+void main()
 {
-    int a,b,c;
-    int count = 1;
-    for (b=c=10;a="- FIGURE?, UMKC,XYZHello Folks,\
-    TFy!QJu ROo TNn(ROo)SLq SLq ULo+\
-    UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\
-    NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
-    HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
-    T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\
-    Hq!WFs XDt!" [b+++21]; )
-    for(; a-- > 64 ; )
-    putchar ( ++c=='Z' ? c = c/ 9:33^b&1);
-    return 0;
+printf("Range of short int is from %d to %d\n",SHRT_MIN,SHRT_MAX);
+printf("Range of int is from %d to %d\n",INT_MIN,INT_MAX);
 }
