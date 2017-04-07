062cb47344d77aad2b074d0395b066196b2c5e29
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-04-07 21:35:34 +0530

    Q4

--- Manmeet Singh/C Questions/Q3.c
+++ Manmeet Singh/C Questions/Q3.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	float n;
-	double d;
-	printf("Enter the Number to be converted from Float to double\n");
-	scanf("%f",&n);
-	d=(double)n;
-	printf("The Double form of %f Float type is %lf\n",n,d);
+	float d;
+	double n;
+	printf("Enter the Number to be converted from Double to Float\n");
+	scanf("%lf",&n);
+	d=(float)n;
+	printf("The Float form of %lf Double type is %f\n",n,d);
 }
---
+++ Manmeet Singh/C Questions/Q4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int   a;
+float  b;
+char  c;
+short  d;
+long  e;
+double  f;
+long double  g;
+printf("Size of intdatatype is = %ld\n",sizeof(a));
+printf("Size of float datatype is = %ld\n",sizeof(b));
+printf("Size of char datatype is = %ld\n",sizeof(c));
+printf("Size of short datatype is = %ld\n",sizeof(d));
+printf("Size of long datatype is = %ld\n",sizeof(e));
+printf("Size of double datatype is = %ld\n",sizeof(f));
+printf("Size of long double datatype is = %ld",sizeof(g));
+}
+
