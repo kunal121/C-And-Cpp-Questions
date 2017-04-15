7cabb0aa785f5062054a212aa57db781c2a30818
Nikhil Bhaskar <perfectnikhil1697@gmail.com>
2017-04-15 21:03:33 +0530

    Update algo-imp-3.cpp

--- Nikhil Bhaskar/algo-imp-3.cpp
+++ Nikhil Bhaskar/algo-imp-3.cpp
@@ -45,13 +45,23 @@ int main(){
    }*/
 
 
-     if(x2>x1 && v2>v1)
+   while(x1>=0)
+        {
+        x1=x1+v1;
+        x2=x2+v2;
+        if(x1==x2)
+            {
+            count++;
+        }
+    }
+    if(count>=1)
+        {
+         cout<<"YES";
+        
+    }
+    else
         cout<<"NO";
-    else if(x1<x2 && v1>v2)
-        cout<<"YES";
-       else if(x2<x1 && v2>v1)
-        cout<<"YES";
-        else
-        cout<<"NO";
+  
+    
     return 0;
 }
