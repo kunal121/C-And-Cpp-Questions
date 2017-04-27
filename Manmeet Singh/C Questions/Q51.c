d2de4ce871b338d6f6ba19bce42b0ec43b9ea2a5
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-04-27 11:47:14 +0530

    Q51

---
+++ Manmeet Singh/C Questions/Q51.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{ FILE *fptr;
+char filename[15];
+charch;
+printf("Enter the filename to be opened \n");
+scanf("%s", filename);
+    /*  open the file for reading */
+fptr = fopen(filename, "r");
+if (fptr == NULL)
+    {
+printf("Cannot open file \n");
+exit(0);
+    }
+ch = fgetc(fptr);
+while (ch != EOF)
+    {   printf ("%c", ch);
+ch = fgetc(fptr);    }
+fclose(fptr);}
