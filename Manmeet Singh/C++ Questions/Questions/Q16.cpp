ace2145dc1bf740fd882200bb8b29f4c393a6a98
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-05-06 11:21:35 +0530

    Q16

---
+++ Manmeet Singh/C++ Questions/Questions/Q16.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{	int x=-1;
+	try
+	{
+	    if(x<0)
+			throw x;
+	}
+	catch(int)
+	{
+	    cout<<"Negative value exception\n";
+	}
+	cout<<"After catch.\n";
+
+}
