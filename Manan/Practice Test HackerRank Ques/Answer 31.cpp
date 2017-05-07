d95cec99b787c12e8ac01f9cb649b3b0bc0b97b4
Manan Garg <manangarg899@gmail.com>
2017-05-07 21:59:55 +0530

    Answer 31

---
+++ Manan/Practice Test HackerRank Ques/Answer 31.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+#include<algorithm>
+template <class T>
+void func(vector<T> &v)
+{
+    int a=v.size();
+    int i;
+    sort(v.begin(),v.end());
+    for(i=0;i<a;i++)
+        {
+        cout<<v[i]+1<<" ";
+    }
+    cout<<endl;
+    for(i=0;i<a;i++)
+        {
+        cout<<v[i]*3<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    int N;
+    cin>>N;
+    vector<int>v;
+    for(int i=0;i<N;i++)
+        {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    func<int>(v);
+    vector<double>v1;
+    for(int i=0;i<N;i++)
+        {
+        double x;
+        cin>>x;
+        v1.push_back(x);
+    }
+    func<double>(v1);
+    return 0;
+}
