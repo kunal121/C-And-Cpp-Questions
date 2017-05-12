d3e4e45fbf39daac877134841ba7e3b3581ab2a2
mohit472 <aryashi4@gmail.com>
2017-05-12 21:12:24 +0530

    string 1 in string 2 in c

---
+++ Mohit Bansal/C_programs/str1in2between.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main()
+{
+	char a[10];
+	char b[10];
+	char c[10];
+	int p=0,r=0,i=0;
+	int t=0;
+	int x,g,s,n,o;
+	clrscr();
+
+	puts("Enter First String:");
+	gets(a);
+	puts("Enter Second String:");
+	gets(b);
+	for(i=0;b[i]!='\0';i++)
+    {
+        if(b[i]==' ')
+        {
+            while(i <= r)
+	{
+		c[i]=a[i];
+		i++;
+	}
+	x = c[i];
+		if(t<n)
+		{
+			a[i] = b[t];
+			t=t+1;
+		}
+		a[o]=x;
+		o=o+1;
+	}	s = n+r;
+	o = p+n;
+
+ // Adding the sub;
+        }
+    }
+	r = strlen(a);
+	n = strlen(b);
+	i=0;
+
+ // Copying the input string into another array
+-string
+
+
+	printf("%s", a);
+	getch();
+}
