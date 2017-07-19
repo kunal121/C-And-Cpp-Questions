6561d584380e3f73b1a5b4de1b5739b8c2419046
Mohit <aryashi4@gmail.com>
2017-07-19 23:02:37 +0530

    calculation of profit

---
+++ Mohit Bansal/C_programs/profit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int whPr,retPr,profit;
+printf("Enter the value of Wholesale price,\n Enter the value of Retail price");
+scanf("%d%d",&whPr,&retPr);
+profit=retPr-whPr;
+printf("profit is %d \n",profit);
+}
