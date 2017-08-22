6ff58d60a6028bb3b6ae953215ef1ce54883ee7a
ROMAN <kartikryder@gmail.com>
2017-08-23 01:16:19 +0530

    switch vowel c-basics

---
+++ Kartik/switchvowel.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void main()
+{
+char vow;
+printf("Enter any alphabet");
+scanf("%c",&vow);
+switch(vow)
+{
+    case('a'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('e'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('i'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('o'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('u'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('A'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('E'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('I'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('O'):
+    printf("this alphabet is vowel");
+    break;
+
+    case('U'):
+    printf("this alphabet is vowel");
+    break;
+
+    default:
+    printf("this alphabet is not vowel");
+    break;
+
+
+}
+}
