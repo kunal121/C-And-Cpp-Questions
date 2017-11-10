a6f32862673d4694f90f48fae803c7fac044ee41
ROMAN <kartikryder@gmail.com>
2017-11-10 15:12:22 +0530

    swap 3 variables using call by reference cpp-basics

---
+++ Kartik/swapcallbyrefrence3var.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void disp(int &a,int &b);
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    disp(a,b);
+}
+void disp(int &a,int &b)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"Swapped Numbers are"<<a<<" "<<b;
+}
